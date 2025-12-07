d84d7d06d265c13914b59329cac02626decd39f3
btcnedir <ttajans@hotmail.com>
2025-12-07 16:04:06 +0300

    test deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
+    return (
+        {/* test deploy VS2022 */ }
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
@@ -63,4 +64,3 @@ export default function Home() {
     </div>
   );
 }
-{/* test deploy VS2022 */ }
